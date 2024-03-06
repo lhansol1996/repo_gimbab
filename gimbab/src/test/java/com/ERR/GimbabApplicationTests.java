63bdbe82e3596a3aadbb69554288d3bce440f8d4
lhs1996 <lhs1996@gmail.com>
2024-03-06 15:44:49 +0900

    20240306 share

---
+++ gimbab/src/test/java/com/ERR/GimbabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ERR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimbabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

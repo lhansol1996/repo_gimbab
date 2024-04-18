d136dad340f824f0559f8d969a674e1e4a0f7139
lhansol1996 <lhs1996@gmail.com>
2024-04-18 11:24:46 +0900

    record 1

--- gimbab/src/main/java/com/ERR/common/base/RecordController.java
+++ gimbab/src/main/java/com/ERR/common/base/RecordController.java
@@ -1,7 +1,6 @@
 package com.ERR.common.base;
 
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,41 +21,34 @@ public class RecordController extends BaseController {
 	CodeService codeService;
 
 	@RequestMapping(value = "/record")
-	public String record(@RequestParam("nickName") String nickName,Model model) throws Exception {
-		
-		//userNum 호출 API
-		String getUserNumUrl = "https://open-api.bser.io/v1/user/nickname?query=" + URLEncoder.encode(nickName, "UTF-8");
-		
+	public String record(@RequestParam("nickName") String nickName, Model model) throws Exception {
+
+		// userNum 호출 API
+		String getUserNumUrl = "https://open-api.bser.io/v1/user/nickname?query="
+				+ URLEncoder.encode(nickName, "UTF-8");
+
 		UtilApi.callERApi(getUserNumUrl);
-		// API 호출 및 stringBuilder 받아오기
-	    StringBuilder stringBuilder = UtilApi.callERApi(getUserNumUrl);
-	    
-		//objectMapper 생성 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> getUserMap = objectMapper.readValue(stringBuilder.toString(), Map.class);
+		StringBuilder stringBuilder = UtilApi.callERApi(getUserNumUrl);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object> getUserMap = objectMapper.readValue(stringBuilder.toString(), Map.class);
 		System.out.println(getUserMap);
 		Map<String, Object> getUserNumMap = (Map<String, Object>) getUserMap.get("user");
 		String userNum = String.valueOf(getUserNumMap.get("userNum"));
-		System.out.println("========");
-		System.out.println("========");
-		//받은 userNum 으로 전적 호출 API
-		String getUserRecordUrl = "https://open-api.bser.io/v1/user/games/"+userNum; 
+
+		// 받은 userNum 으로 전적 호출 API
+		String getUserRecordUrl = "https://open-api.bser.io/v1/user/games/" + userNum;
 		UtilApi.callERApi(getUserRecordUrl);
 		StringBuilder stringBuilder2 = UtilApi.callERApi(getUserRecordUrl);
-		//objectMapper 생성 
-        ObjectMapper objectMapper2 = new ObjectMapper();
-        Map<String, Object> getUserRecord = objectMapper2.readValue(stringBuilder2.toString(), Map.class);
-//        System.out.println(getUserRecord);
-    	System.out.println("getUserRecord========");
-		System.out.println("========");
+		// objectMapper 생성
+		ObjectMapper objectMapper2 = new ObjectMapper();
+		Map<String, Object> getUserRecord = objectMapper2.readValue(stringBuilder2.toString(), Map.class);
 		List<Map<String, Object>> userGamesList = (List<Map<String, Object>>) getUserRecord.get("userGames");
-		
+
 		model.addAttribute("userGamesList", userGamesList);
-	
-		        // 필요한 정보를 여기서 처리합니다
-	
-		
+
 		return UsrCommonPath + "record";
 	}
 
+	
 }
